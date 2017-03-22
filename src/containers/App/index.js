f4171c946066c8c96e72febd0a62c7c6dca6adf2
p-le <p-le@dimage.co.jp>
2017-03-22 19:03:00 +0900

    add react-router

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,5 +1,36 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
-const App = () => <div>Hello World</div>;
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/test">Test</Link></li>
+      </ul>
+      <hr />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/test" render={() => <div>Test</div>} />
+    </div>
+  </Router>
+);
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
 
 export default App;
